c806ddeea5cb66c6e8b2715b2bc4117b014964d7
Ike <ike.mattice@tiger-sheep.com>
2016-07-11 10:45:27 -0700

    handle case where user submits a new search request before the previous request is completed.

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -12,6 +12,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchResultsTableView: UITableView!
     
+    var dataTask: NSURLSessionDataTask?
     var searchResults = [SearchResult]()
     var hasSearched = false
     var isLoading = false
@@ -210,6 +211,7 @@ extension SearchViewController: UISearchBarDelegate {
         if !searchBar.text!.isEmpty {
             searchBar.resignFirstResponder()
             
+            dataTask?.cancel()
             isLoading = true
             searchResultsTableView.reloadData()
             
@@ -218,11 +220,11 @@ extension SearchViewController: UISearchBarDelegate {
             
             let url = urlWithSearchText(searchBar.text!)
             let session = NSURLSession.sharedSession()
-            let dataTask = session.dataTaskWithURL(url, completionHandler: {
+            dataTask = session.dataTaskWithURL(url, completionHandler: {
                 data, response, error in
                 
-                if let error = error {
-                    print("Failure! \(error)")
+                if let error = error where error.code == -999 {
+                    return
                 } else if let httpResponse = response as? NSHTTPURLResponse where httpResponse.statusCode == 200 {
                     if let data = data, dictionary = self.parseJSON(data) {
                         self.searchResults = self.parseDictionary(dictionary)
@@ -246,7 +248,7 @@ extension SearchViewController: UISearchBarDelegate {
                 }
             })
             
-            dataTask.resume()
+            dataTask?.resume()
         }
     }
     
